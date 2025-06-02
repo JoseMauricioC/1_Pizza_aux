f634d80a86b06171a012276bf69515fcf759b563
JoseMauricioC <josemauriciocamacho1@gmail.com>
2025-06-01 21:43:10 -0300

    falta la opcion de editar en el front

--- frontend/src/views/ClienteView.vue
+++ frontend/src/views/ClienteView.vue
@@ -1,11 +1,34 @@
 <script setup lang="ts">
 import ClienteList from '@/components/cliente/ClienteList.vue'
+import ClienteSave from '@/components/cliente/ClienteSave.vue'
+import { Button } from 'primevue'
+import { ref } from 'vue'
+
+const mostrarDialog = ref(false)
+const clienteListRef = ref<typeof ClienteList | null>(null)
+
+function handleCreate() {
+  mostrarDialog.value = true
+}
+
+function handleGuardar() {
+  clienteListRef.value?.obtenerLista()
+}
+function handleCloseDialog() {
+  mostrarDialog.value = false
+}
 </script>
 
 <template>
   <div>
     <h1>Clientes</h1>
-    <ClienteList />
+    <Button label="Crear Nuevo" icon="pi pi-plus" @click="handleCreate" />
+    <ClienteList ref="clienteListRef" />
+    <ClienteSave
+      :mostrar="mostrarDialog"
+      @guardar="handleGuardar"
+      @close="handleCloseDialog"
+    />
   </div>
 </template>
 
