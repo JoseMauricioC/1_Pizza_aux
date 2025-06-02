5b006700b0295483e4d757f90b685e4f55e30c6f
JoseMauricioC <josemauriciocamacho1@gmail.com>
2025-06-01 21:11:58 -0300

    eliminar en el front falta editar, crear

--- frontend/src/components/cliente/ClienteList.vue
+++ frontend/src/components/cliente/ClienteList.vue
@@ -1,14 +1,27 @@
 <script setup lang="ts">
 import type { Cliente } from '@/models/cliente'
 import http from '@/plugins/axios'
-import { Button } from 'primevue'
+import { Button, Dialog } from 'primevue'
 import { onMounted, ref } from 'vue'
 
 const ENDPOINT = 'clientes'
 const clientes = ref<Cliente[]>([])
+const clienteDelete = ref<Cliente | null>(null)
+const mostrarConfirmDialog = ref<boolean>(false)
 
 async function obtenerLista() {
   clientes.value = await http.get(ENDPOINT).then((response) => response.data)
+}
+
+function mostrarEliminarConfirm(cliente: Cliente) {
+  clienteDelete.value = cliente
+  mostrarConfirmDialog.value = true
+}
+
+async function eliminar() {
+  await http.delete(`${ENDPOINT}/${clienteDelete.value?.id}`)
+  obtenerLista()
+  mostrarConfirmDialog.value = false
 }
 
 onMounted(() => {
@@ -39,11 +52,35 @@ defineExpose({ obtenerLista })
           <td>{{ cliente.direccion }}</td>
           <td>
             <Button icon="pi pi-pencil" aria-label="Editar" text />
-            <Button icon="pi pi-trash" aria-label="Eliminar" text severity="danger" />
+            <Button
+              icon="pi pi-trash"
+              aria-label="Eliminar"
+              text
+              severity="danger"
+              @click="mostrarEliminarConfirm(cliente)"
+            />
           </td>
         </tr>
       </tbody>
     </table>
+
+    <Dialog
+      v-model:visible="mostrarConfirmDialog"
+      header="Confirmar Eliminación"
+      :style="{ width: '25rem' }"
+    >
+      <p>¿Estás seguro de que deseas eliminar este registro?</p>
+      <br />
+      <div class="flex justify-end gap-2">
+        <Button
+          type="button"
+          label="Cancelar"
+          severity="secondary"
+          @click="mostrarConfirmDialog = false"
+        />
+        <Button type="button" label="Eliminar" @click="eliminar" />
+      </div>
+    </Dialog>
   </div>
 </template>
 
